ddfcf7e54281186aac97c7b3c993ea51b777a2de
sele14 <eirik.selander@gmail.com>
2020-12-16 19:17:16 +0000

    moved layout separate file

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import dash_table
 import pandas as pd
 import plotly.graph_objects as go
 
+# impoty layout component
+from layout import layout
+
 # Module
 from analytics import totGoalsLine
 
@@ -19,54 +22,7 @@ app = dash.Dash(__name__,
 
 app.title = "Football Stats"
 
-app.layout = html.Div([
-    html.H6("Enter the name of a player"),
-	html.Div([
-		html.Div([
-		html.Div(
-			[dcc.Input(id='my-input',
-						value='Eden Hazard', type='text')
-			], className='button1'),
-		html.Div(html.Button('Submit', id='button1', n_clicks=0)),
-		],className='two columns'),
-		html.Div([
-			dcc.Dropdown(
-				id="dropdown",
-				options=[
-					{'label': 'Goals', 'value': 'Goals'},
-					{'label': 'Assists', 'value': 'Assists'},
-					{'label': 'Games Played', 'value': 'Games Played'}
-				],
-				value='Goals',
-				clearable=False
-			)
-		], className='two columns')
-	],className='row'),
-    html.Br(),
-	# Goals Graph
-	html.Div(
-		dcc.Graph(
-			id='totgoals',
-			figure={
-				'layout': {
-                'plot_bgcolor': 'rgb(0, 12, 39)',
-				'paper_bgcolor': 'rgb(0, 12, 39)',
-				}
-			}
-
-			),style={'backgroundColor' : 'rgb(0, 12, 39)'}
-	),
-
-	# Table
-    html.Div(id='my-output'),
-
-	# Store data as hidden div (or dcc.Store?)
-	html.Div(id='stats-data', 
-			style={"display" : 'none'}
-			)
-
-])
-
+app.layout = layout()
 
 @app.callback(
     Output('stats-data', 'children'),
